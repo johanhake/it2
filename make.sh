13ece32abb49269e8ea398d28328a9b80ad0e21d
Johan Hake <hake.dev@gmail.com>
2015-08-10 15:32:26 +0200

    Added building of laereplan as a standalone pdf

--- make.sh
+++ make.sh
@@ -1,17 +1,37 @@
 DOCNAME=it2
 TEXDOC=$DOCNAME.tex
 BOOTWATCHSTYLE=journal # Check out https://bootswatch.com
+
+# Compile it2 document using bootstrap theme
 doconce format html $DOCNAME --html_style=bootswatch_$BOOTWATCHSTYLE --html_admon=bootstrap_panel --html_bootstrap_jumbotron=off --html_bootstrap_navbar=on --encoding=utf-8 --pygments_html_linenos --examples_as_exercises
 doconce split_html $DOCNAME
 
+# Compile it2 document using solarized theme
 doconce format html $DOCNAME --html_output=$DOCNAME-solarized --html_style=solarized --encoding=utf-8 --pygments_html_linenos
 doconce split_html $DOCNAME-solarized
 
+# Compile it2 document to pdf
 doconce format pdflatex $DOCNAME --encoding=utf-8 --latex_admon=mdfbox --latex_font=palatino --latex_papersize=a4 --latex_admon_title_no_period --no_ampersand_quote --pygments_html_linenos --examples_as_exercises
 doconce ptex2tex $DOCNAME envir=minted 
 doconce replace 'linecolor=black,' 'linecolor=blue!80!black!20,' $TEXDOC
 doconce replace 'background}{gray!5' 'background}{yellow!30' $TEXDOC
-doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=yellow!45,' it2.tex
+doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=yellow!45,' $TEXDOC
+
+# Fix bug when not using springer styles
+doconce replace '\usepackage{lmodern}' '%\usepackage{lmodern}' $TEXDOC
+
+# Fix some page settings
+doconce replace '10pt]' '12pt]' $TEXDOC
+doconce replace '\usepackage[a4paper]{geometry}' '\usepackage[a4paper, margin=1in]{geometry}' $TEXDOC
+
+# Compile laereplan document to pdf
+DOCNAME=laereplan_it2
+TEXDOC=$DOCNAME.tex
+doconce format pdflatex $DOCNAME --encoding=utf-8 --latex_admon=mdfbox --latex_font=palatino --latex_papersize=a4 --latex_admon_title_no_period --no_ampersand_quote --pygments_html_linenos --examples_as_exercises
+doconce ptex2tex $DOCNAME envir=minted 
+doconce replace 'linecolor=black,' 'linecolor=blue!80!black!20,' $TEXDOC
+doconce replace 'background}{gray!5' 'background}{yellow!30' $TEXDOC
+doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=yellow!45,' $TEXDOC
 
 # Fix bug when not using springer styles
 doconce replace '\usepackage{lmodern}' '%\usepackage{lmodern}' $TEXDOC
@@ -23,6 +43,7 @@ doconce replace '\usepackage[a4paper]{geometry}' '\usepackage[a4paper, margin=1i
 pdflatex -shell-escape $TEXDOC
 pdflatex -shell-escape $TEXDOC
 
+# Update kode zip file
 rm kode.zip
 cd gist
 zip -r ../kode.zip eksempler oppgaver --exclude */.git
