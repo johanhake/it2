08585f29ae41355641a7a89aae47583c82ce9be3
Johan Hake <hake.dev@gmail.com>
2015-08-10 16:00:26 +0200

    Added changes to laereplan

--- make.sh
+++ make.sh
@@ -24,24 +24,10 @@ doconce replace '\usepackage{lmodern}' '%\usepackage{lmodern}' $TEXDOC
 doconce replace '10pt]' '12pt]' $TEXDOC
 doconce replace '\usepackage[a4paper]{geometry}' '\usepackage[a4paper, margin=1in]{geometry}' $TEXDOC
 
-# Compile laereplan document to pdf
-DOCNAME=laereplan_it2
-TEXDOC=$DOCNAME.tex
-doconce format pdflatex $DOCNAME --encoding=utf-8 --latex_admon=mdfbox --latex_font=palatino --latex_papersize=a4 --latex_admon_title_no_period --no_ampersand_quote --pygments_html_linenos --examples_as_exercises
-doconce ptex2tex $DOCNAME envir=minted 
-doconce replace 'linecolor=black,' 'linecolor=blue!80!black!20,' $TEXDOC
-doconce replace 'background}{gray!5' 'background}{yellow!30' $TEXDOC
-doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=yellow!45,' $TEXDOC
-
-# Fix bug when not using springer styles
-doconce replace '\usepackage{lmodern}' '%\usepackage{lmodern}' $TEXDOC
-
-# Fix some page settings
-doconce replace '10pt]' '12pt]' $TEXDOC
-doconce replace '\usepackage[a4paper]{geometry}' '\usepackage[a4paper, margin=1in]{geometry}' $TEXDOC
-
-pdflatex -shell-escape $TEXDOC
-pdflatex -shell-escape $TEXDOC
+# Compile laereplanen
+pushd laereplan_it
+bash make.sh
+popd
 
 # Update kode zip file
 rm kode.zip
