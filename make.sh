cb9e0b65aeea9f9941b82feb08467dcc448dd3f0
Johan Hake <hake.dev@gmail.com>
2015-08-17 15:52:02 +0200

    Update with new exercise

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ TEXDOC=$DOCNAME.tex
 BOOTWATCHSTYLE=journal # Check out https://bootswatch.com
 
 # Compile it2 document using bootstrap theme
-doconce format html $DOCNAME --html_style=bootswatch_$BOOTWATCHSTYLE --html_admon=bootstrap_panel --html_bootstrap_jumbotron=off --html_bootstrap_navbar=on --encoding=utf-8 --pygments_html_linenos --examples_as_exercises  --html_toc_depth=3  --without_solutions
+doconce format html $DOCNAME --html_style=bootswatch_$BOOTWATCHSTYLE --html_admon=bootstrap_panel --html_bootstrap_jumbotron=off --html_bootstrap_navbar=on --encoding=utf-8 --pygments_html_linenos --html_toc_depth=3  --without_solutions
 doconce replace '<a href="laereplan_it.pdf" target="_self">PDF versjon av dette dokumentet (for printing)</a>' '<a href="laereplan_it/laereplan_it.pdf" target="_self">PDF versjon av dette dokumentet (for printing)</a>' $DOCNAME.html
 
 doconce split_html $DOCNAME
@@ -14,9 +14,9 @@ doconce format html $DOCNAME --html_output=$DOCNAME-solarized --html_style=solar
 doconce split_html $DOCNAME-solarized
 
 # Compile it2 document to pdf
-doconce format pdflatex $DOCNAME --encoding=utf-8 --latex_admon=mdfbox --latex_font=palatino --latex_papersize=a4 --latex_admon_title_no_period --no_ampersand_quote --pygments_html_linenos --examples_as_exercises --solutions_at_end --without_solutions
+doconce format pdflatex $DOCNAME --encoding=utf-8 --latex_admon=mdfbox --latex_font=palatino --latex_papersize=a4 --latex_admon_title_no_period --no_ampersand_quote --pygments_html_linenos --examples_as_exercises --solutions_at_end --without_solutions --device=paper
 
-doconce ptex2tex $DOCNAME envir=minted 
+doconce ptex2tex $DOCNAME envir=minted --device=paper
 doconce replace 'linecolor=black,' 'linecolor=blue!80!black!20,' $TEXDOC
 doconce replace 'background}{gray!5' 'background}{yellow!30' $TEXDOC
 doconce subst 'frametitlebackgroundcolor=.*?,' 'frametitlebackgroundcolor=yellow!45,' $TEXDOC
